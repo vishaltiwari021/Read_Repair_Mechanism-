01b0d64c9a30b8621b59877af18f82657b333ebd
vishaltiwari021 <vishaltiwarivt021@gmail.com>
2026-03-26 19:11:43 +0530

    vite config

--- frontend/ReadRepair/vite.config.js
+++ frontend/ReadRepair/vite.config.js
@@ -1,9 +1,23 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, ".", "");
 
-})
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      host: "0.0.0.0",
+      port: 5000,
+      allowedHosts: true,
+      proxy: {
+        "/api": {
+          target: env.VITE_PROXY_TARGET || "http://localhost:3000",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  };
+});
